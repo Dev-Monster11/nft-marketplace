601a7429c05df6f277a483a9ead2e33994a3ed8e
Harry Herdiana <harry.gamebot@gmail.com>
2022-02-12 21:20:12 +0700

    moved modal to _app

--- app/packages/web/src/views/landingPage.tsx
+++ app/packages/web/src/views/landingPage.tsx
@@ -103,26 +103,7 @@ export const LandingPageView = () => {
             }
           `}
         </style>
-        <MetaplexOverlay centered visible>
-          <div className="comming_soon_modal">
-            <div className="comming_soon_text">
-              <h1 className="comming_soon_title">Starting soon...</h1>
-              <h4>We are doing final touch ups, you will be entering soon.</h4>
-              <h4>
-                Your 1 hour registration will hold valid, you can come in even
-                after, as soon as we start.
-              </h4>
-              <h4>
-                If you have work or errands to take care in the meantime, please
-                do, we will email you as soon as the event starts.
-              </h4>
-              <h4>
-                Thank you sooo much again for being so patient with a
-                bootstrapped minority founded team.
-              </h4>
-            </div>
-          </div>
-        </MetaplexOverlay>
+
         <div
           className={`landing-page overflow-hidden`}
           style={{ height: '75vh' }}
